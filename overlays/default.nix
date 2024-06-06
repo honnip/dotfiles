c815b6dad8c48743af3f016e70439a51f6de4a3f
Honnip <me@honnip.page>
2024-06-06 17:41:59 +0900

    gnome.mutter: add `text-input-v1` patch

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,13 @@
               rev = "663f19bc02c1b4e3d1a67b4ad72d644f9b9d6970";
               sha256 = "sha256-I1s4yz5JEWJY65g+dgprchwZuPGP9djgYXrMMxDQGrs=";
             };
+            patches = (old.patches or [ ]) ++ [
+              # text-input-v1 support
+              (prev.fetchpatch {
+                url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3751/diffs.patch";
+                hash = "sha256-fuyR+FfPHYGDwyGZ8zzIahqKlQFyguYCc4qQ/iGcizM=";
+              })
+            ];
           });
         }
       );
