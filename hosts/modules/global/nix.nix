1d785ebcadc97759f11246071f818233d537aa24
Honnip <me@honnip.page>
2024-08-12 21:46:20 +0900

    nix: cleanup

--- hosts/modules/global/nix.nix
+++ hosts/modules/global/nix.nix
@@ -4,7 +4,6 @@
     optimise.automatic = true;
     settings = {
       trusted-users = [
-        "root"
         "@wheel"
       ];
       auto-optimise-store = lib.mkDefault true;
@@ -14,20 +13,12 @@
       ];
       substituters = [
         "https://cache.lix.systems"
-        "https://cache.nixos.org"
-        "https://helix.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      # Keep the last 5 generations
-      options = "--delete-older-than +5";
     };
   };
 }
