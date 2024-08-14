91bff766b8c795a838148cc5ec74c2dace444115
Honnip <me@honnip.page>
2024-08-14 19:48:52 +0900

    lix: 2.90.0 -> 2.91.0

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
