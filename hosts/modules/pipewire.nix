15e1f6caeb5f7773804f80009ae1bb5001bbc985
Honnip <me@honnip.page>
2024-09-03 23:41:33 +0900

    pipewire: remove support for alsa, jack

--- hosts/modules/pipewire.nix
+++ hosts/modules/pipewire.nix
@@ -3,11 +3,6 @@
   hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
     pulse.enable = true;
-    jack.enable = true;
   };
 }
