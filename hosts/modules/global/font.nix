7ca3c96cc5c2a1a13d5f5a4f33f975e72adbc1ac
Honnip <me@honnip.page>
2024-10-23 16:46:15 +0900

    update lock

--- hosts/modules/global/font.nix
+++ hosts/modules/global/font.nix
@@ -3,7 +3,8 @@
   fonts.packages = with pkgs; [
     fira-code
     noto-fonts
-    noto-fonts-cjk
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
     noto-fonts-color-emoji
     iosevka
     ibm-plex
