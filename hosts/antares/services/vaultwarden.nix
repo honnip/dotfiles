fb5e6e2417dea4bb0d185ddc38e2a650357d6daa
Honnip <me@honnip.page>
2024-11-04 22:29:29 +0900

    vaultwarden: migrate to postgresql

--- hosts/antares/services/vaultwarden.nix
+++ hosts/antares/services/vaultwarden.nix
@@ -6,11 +6,25 @@ in
   services = {
     vaultwarden = {
       enable = true;
+      dbBackend = "postgresql";
       config = {
         DOMAIN = "https://" + base;
-        ROCKET_PORT = 8000;
         SIGNUPS_ALLOWED = false;
+
+        DATABASE_URL = "postgresql:///vaultwarden";
+        ROCKET_PORT = 8000;
       };
     };
+  };
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "vaultwarden" ];
+    ensureUsers = [
+      {
+        name = "vaultwarden";
+        ensureDBOwnership = true;
+      }
+    ];
   };
 }
