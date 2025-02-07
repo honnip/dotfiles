23994f1ffb0bec345442175917d92a02e641fbcc
Honnip <me@honnip.page>
2025-02-07 16:35:42 +0900

    helix: nixd setup

--- home/honnip/modules/cli/helix.nix
+++ home/honnip/modules/cli/helix.nix
@@ -44,6 +44,13 @@
       language-server = {
         nixd = {
           command = "nixd";
+          config = {
+            nixpkgs.expr = "import \"\${flake.inputs.nixpkgs}\" { }";
+            options = {
+              nixos.expr = "(let pkgs = import \"\${inputs.nixpkgs}\" { }; in (pkgs.lib.evalModules { modules = (import \"\${inputs.nixpkgs}/nixos/modules/module-list.nix\") ++ [ ({...}: { nixpkgs.hostPlatform = builtins.currentSystem; }) ]; })).options";
+              home_manager.expr = "(let pkgs = import \"\${inputs.nixpkgs}\" { }; lib = import \"\${inputs.home-manager}/modules/lib/stdlib-extended.nix\" pkgs.lib; in (lib.evalModules { modules = (import \"\${inputs.home-manager}/modules/modules.nix\") { inherit lib pkgs; check = false; }; })).options";
+            };
+          };
         };
       };
       language = [
