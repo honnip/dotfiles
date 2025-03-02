d8ccb3f42d052c5bd99925a6012c72c7b55e32ca
Honnip <me@honnip.page>
2025-03-02 21:02:46 +0900

    feat(monitor): init victoriametrics

--- hosts/canopus/services/metrics.nix
+++ hosts/canopus/services/metrics.nix
@@ -1,35 +1,38 @@
-let
-  configuration = builtins.toFile "prometheus.yml" ''
-    scrape_configs:
-    - job_name: blocky
-      stream_parse: true
-      static_configs:
-      - targets:
-        - localhost:4000/metrics
-    - job_name: node-exporter
-      stream_parse: true
-      static_configs:
-      - targets:
-        - localhost:9100
-  '';
-in
 {
+  services.victorialogs.enable = true;
   services.victoriametrics = {
     enable = true;
-    extraOptions = [ "-promscrape.config=${configuration}" ];
+    prometheusConfig = {
+      scrape_configs = [
+        {
+          job_name = "node-exporter";
+          static_configs = [
+            {
+              targets = [
+                "localhost:9100"
+                "antares:9100"
+                "acrux:9100"
+              ];
+              labels.type = "node";
+            }
+          ];
+        }
+      ];
+    };
   };
 
   services.grafana = {
     enable = true;
     settings = {
       server = {
-        http_addr = "127.0.0.1";
-        http_port = 3000;
+        http_addr = "0.0.0.0";
+        http_port = 4000;
       };
+      "auth.anonymous".enabled = true;
     };
     provision.datasources.settings.datasources = [
       {
-        name = "victoria-metric";
+        name = "victoria";
         url = "http://localhost:8428";
         type = "prometheus";
       }
