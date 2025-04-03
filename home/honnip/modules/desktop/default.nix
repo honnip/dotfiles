2d07f01f92ab99d08e391f41b21a9be43e7a68a4
Honnip <me@honnip.page>
2025-04-03 13:47:19 +0900

    josm: init with wayland support

--- home/honnip/modules/desktop/default.nix
+++ home/honnip/modules/desktop/default.nix
@@ -23,6 +23,18 @@
 
     fragments
     rquickshare
+
+    (josm.override {
+      jre = jdk21.overrideAttrs {
+        src = pkgs.fetchFromGitHub {
+          owner = "openjdk";
+          repo = "wakefield";
+          rev = "16cb66c24910f24be44ac19949a8933c8afba848";
+          hash = "sha256-sQ63/zPKOk2d0hBt5uVVaZT1RZKOaBwuXp/b8DVa5mI=";
+        };
+      };
+      extraJavaOpts = "-Djosm.restart=true -Djava.net.useSystemProxies=true -Dawt.toolkit.name=WLToolkit -Dsun.java2d.vulkan=true";
+    })
   ];
 
   xdg.mimeApps.defaultApplications = {
