f697aa19994bfe1d48ef52596c25a1f39dfd6b4f
Honnip <me@honnip.page>
2026-07-13 09:11:48 +0900

    update lock, downgrade spicetify-cli
    waiting nixpkgs#540416 lands

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,4 +6,17 @@
       inherit inputs;
       pkgs = final;
     };
+
+  downgrade-spiectify = final: prev: {
+    spicetify-cli = prev.spicetify-cli.overrideAttrs (old: {
+      version = "2.43.2";
+      src = prev.fetchFromGitHub {
+        owner = "spicetify";
+        repo = "cli";
+        tag = "v2.43.2";
+        hash = "sha256-77OZVDtybkYI5R3tZ7q2cLJ+Ixn8WB4CP4qP6Yp535g=";
+      };
+      vendorHash = "sha256-uuvlu5yocqnDh6OO5a4Ngp5SahqURc/14fcg1Kr9sec=";
+    });
+  };
 }
